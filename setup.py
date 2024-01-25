3098df22d3420c7f19a5cfdb91609c5399c8f0e3
Diego Armando Barquero Sánchez <126104692+barquerosanchezdiegoarmando@users.noreply.github.com>
2024-01-25 15:03:50 -0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+# setup.py
+
+from setuptools import setup
+
+setup(
+    name="econ-simulation",
+    version="1.0.0",
+    python_requires=">=3.6"
+    author="Diego Armando Barquero Sanchez",
+    author_email="barquerosanchezdiego@gmail.com",
+    description="Una libreria simple en pre-alpha creada para facilitar la computacion y ploteo de ejercicio microeconomicos",
+    url="https://github.com/barquerosanchezdiegoarmando/econ-simulation",
+    packages=["econ-simulation"],
+    install_requires=["google-colab",'pandas','numpy','scipy','scipy.optimize','matplotlib.pyplot'],
+)
