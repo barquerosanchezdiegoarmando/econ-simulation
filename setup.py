3051c157f761bd0984af6a7b5523ae1016e8ac24
Diego Armando Barquero Sánchez <126104692+barquerosanchezdiegoarmando@users.noreply.github.com>
2024-01-25 19:41:42 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,16 +5,11 @@ setup(
     name='econsimulation',
     version='0.1.0',
     packages=find_packages(),
+    py_modules=['econsimulation.py']
     install_requires=[
         'matplotlib',
         'scipy',
         'numpy',
         'pandas',
         'google-colab'
-    ],
-    entry_points={
-        'console_scripts': [
-            # Puedes agregar scripts ejecutables aquí si es necesario
-        ],
-    },
-)
+    ])
