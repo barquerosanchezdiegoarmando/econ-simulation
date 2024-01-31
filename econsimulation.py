ec1a3a74d3cbafba805c584cb3e11dd1e4c31287
Diego Armando Barquero Sánchez <126104692+barquerosanchezdiegoarmando@users.noreply.github.com>
2024-01-31 13:13:40 -0600

    Add files via upload

--- econsimulation.py
+++ econsimulation.py
@@ -1,26 +1,32 @@
+import pandas as pd
+import numpy as np
+from scipy.optimize import minimize
+from scipy import optimize
+import matplotlib.pyplot as plt
+
 def max_utilidad_cobb_douglas(alpha,beta,m,p1,p2,do_print=True):
-  def objetivo_cobb_douglas(X):
-      x,y=X
-      return -(x**(alpha)*y**(beta))
-  def restriccion_presupuestaria(x):
-      p=[p1,p2]
-      return -m+p[0]*x[0]+p[1]*x[1]
-  b=(0,None)
-  bnds=(b,b)
-  restriccion=[{'type':'eq', 'fun':restriccion_presupuestaria}]
-  sol=minimize(objetivo_cobb_douglas, (0,0), method="SLSQP", constraints=restriccion, bounds=bnds)
-  optimo=sol.x
-  utilidad = optimo[0]**(alpha)*optimo[1]**(beta)
-
-  return print('La canasta de bienes que maximiza la utilidad de consumidor es (', optimo[0],',', optimo[1],'), con U =', utilidad)
-
+    def objetivo_cobb_douglas(X):
+        x,y=X
+        return -(x**(alpha)*y**(beta))
+    def restriccion_presupuestaria(x):
+        p=[p1,p2]
+        return -m+p[0]*x[0]+p[1]*x[1]
+    b=(0,None)
+    bnds=(b,b)
+    restriccion=[{'type':'eq', 'fun':restriccion_presupuestaria}]
+    sol=minimize(objetivo_cobb_douglas, (0,0), method="SLSQP", constraints=restriccion, bounds=bnds)
+    optimo=sol.x
+    utilidad = optimo[0]**(alpha)*optimo[1]**(beta)
+    
+    return print('La canasta de bienes que maximiza la utilidad de consumidor es (', optimo[0],',', optimo[1],'), con U =', utilidad)
+  
 
 def curva_indiferencia_tangente(y1,y2,alpha,beta,N,x2_max):
     def u_func(x1,x2,alpha, beta):
-      return x1**alpha * x2**beta
+          return x1**alpha * x2**beta
 
     def objective(x1,x2,alpha,u):
-      return u_func(x1,x2,alpha,beta) - u
+          return u_func(x1,x2,alpha,beta) - u
     # = 0 then on indifference curve with utility = u
     # a. utility in (y1,y2)
     u_y = u_func(y1,y2,alpha,beta)
@@ -40,7 +46,8 @@ def curva_indiferencia_tangente(y1,y2,alpha,beta,N,x2_max):
 
     return x1_vec,x2_vec
 
-def graficar_indiferencia(x_1,x_2,m,p1,p2):
+
+def graficar_indiferencia(x_1,x_2,m,p1,p2,x1_vec,x2_vec):
   x=np.arange(0.0, 250, 10)
   y=m/p2-(p1*x/p2)
   fig = plt.figure(figsize=(4,4))
@@ -55,8 +62,10 @@ def graficar_indiferencia(x_1,x_2,m,p1,p2):
   ax.set_xlim([0,m/p1])
   ax.set_ylim([0,m/p2])
   ax.grid(True)
+  
+  
 
-def graficar_2_indiferencias(x_1,x_2,x_1_prima,x_2_prima,m,m_prima,p1,p2,p1_prima,p2_prima):
+def graficar_2_indiferencias(x_1,x_2,x_1_prima,x_2_prima,m,m_prima,p1,p2,p1_prima,p2_prima,x1_vec,x2_vec,x1_vec_prima,x2_vec_prima):
     x=np.arange(0.0, 250, 10)
     y_1=m/p2-(p1*x/p2)
     y_2=m_prima/p2_prima-(p1_prima*x/p2_prima)
@@ -87,4 +96,3 @@ def graficar_2_indiferencias(x_1,x_2,x_1_prima,x_2_prima,m,m_prima,p1,p2,p1_prim
     ax.set_xlim([0,limite/p1_definitivo])
     ax.set_ylim([0,limite/p2_definitivo])
     ax.grid(True)
-  
